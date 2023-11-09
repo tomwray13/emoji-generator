6bf0528a21dfbfd6bcfaf5c5137fde21bb480b18
Tom Ray <tomwray13@gmail.com>
2023-11-09 16:23:17 +0000

    tweak route handler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { EmojiValidationPipe } from './common/emoji-validation/emoji-validation.pipe';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getEmoji(@Query('index', EmojiValidationPipe) index?: number) {
-    return this.appService.getEmoji(index);
+  getEmoji(
+    @Req() request: Request,
+    @Query('index', EmojiValidationPipe) index?: number,
+  ) {
+    return {
+      browser: request.headers['user-agent'],
+      emoji: this.appService.getEmoji(index),
+    };
   }
 }

89c825433015a9530f82505aef1b076527959f59
Tom Ray <tomwray13@gmail.com>
2023-11-19 20:17:33 +0000

    Use config module and update start script

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+  console.log(`PORT`, port);
+  const host = `0.0.0.0`;
+  await app.listen(port, host);
 }
 bootstrap();

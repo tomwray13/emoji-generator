7c0d4c8d8942ea1f3fe9d49a18dc91b8d0e7c57c
tomwray13 <tomwray13@gmail.com>
2023-12-03 19:58:14 +0000

    Deploy to railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { AuthGuard } from './common/auth/auth.guard';
 import { BrowserInterceptor } from './common/browser/browser.interceptor';
 import { TransformResponseInterceptor } from './common/transform-response/transform-response.interceptor';
 import { AllExceptionsFilter } from './common/all-exceptions/all-exceptions.filter';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [
     AppService,
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.listen(3000);
+  const port = process.env.PORT || 3000;
+  const host = `0.0.0.0`;
+  app.listen(port, host);
 }
 bootstrap();
